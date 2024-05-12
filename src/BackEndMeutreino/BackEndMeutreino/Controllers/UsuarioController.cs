bd57c6058fd3ef58f2bbd79128c0c479c6238860
natsol01 <natsol2000@gmail.com>
2024-05-12 18:47:41 -0300

    Update historico de peso

--- src/BackEndMeutreino/BackEndMeutreino/Controllers/UsuarioController.cs
+++ src/BackEndMeutreino/BackEndMeutreino/Controllers/UsuarioController.cs
@@ -43,5 +43,33 @@ namespace BackEndMeutreino.Controllers
         {
             return View();
         }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Historico()
+        {
+            var claims = ((ClaimsIdentity)User.Identity).Claims;
+
+            // Encontra a claim de email do usuário
+            var email = claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
+
+            // Encontra o usuário no banco de dados
+            var usuario = repository.GetUsuario(email.Value);
+            var historico = repository.GetHistoricoPeso(usuario.id);
+            var data = new
+            {
+                labels = historico.Select(entry => entry.data), // Supondo que "Date" é a propriedade que armazena a data no histórico de peso
+                weights = historico.Select(entry => entry.peso) // Supondo que "Weight" é a propriedade que armazena o peso no histórico de peso
+            };
+
+            // Envie os dados como JSON
+            return Json(data);
+        }
+
+        [Authorize]
+        public IActionResult HistoricoPeso()
+        {
+            return View();
+        }
     }
 }
