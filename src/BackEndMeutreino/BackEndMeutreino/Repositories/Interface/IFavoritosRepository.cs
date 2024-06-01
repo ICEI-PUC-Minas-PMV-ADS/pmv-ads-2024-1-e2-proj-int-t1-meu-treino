ad33a14acdd2c4219ea11974da330b8efdbcec76
Paula Haru <paulacarinaa97@gmail.com>
2024-06-01 16:06:27 -0300

    add favoritos

--- src/BackEndMeutreino/BackEndMeutreino/Repositories/Interface/IFavoritosRepository.cs
+++ src/BackEndMeutreino/BackEndMeutreino/Repositories/Interface/IFavoritosRepository.cs
@@ -1,11 +1,12 @@
 ﻿using BackEndMeutreino.Models;
+using System.Collections.Generic;
 
 namespace BackEndMeutreino.Repositories.Interface
 {
     public interface IFavoritosRepository
     {
         void Add(Favoritos favoritos);
-
+        List<Favoritos> GetFavoritosByUsuario(int idUsuario);
         Task<bool> saveChangesAsync();
     }
 }
